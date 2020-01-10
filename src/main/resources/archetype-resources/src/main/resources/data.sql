cec0350c0a73b0a2f13b4ef3cf575a8180e78ae7
Tobias Harms <tobias.harms@zalando.de>
2020-01-10 11:52:44 +0100

    added more data and changed existing data to fit better into the model

--- src/main/resources/archetype-resources/src/main/resources/data.sql
+++ src/main/resources/archetype-resources/src/main/resources/data.sql
@@ -8,10 +8,13 @@ insert into CGroup (id, name)
 values(3, 'vehicle');
 
 insert into item(id, name, cgroup, description)
-values(10001, 'in28minutes', 2, 'Learn JPA');
+values(10001, 'Country house', 2, 'Country house with big garden');
 
 insert into item(id, name, cgroup,description)
-values(10002, 'in28minutes', 3, 'Learn Data JPA');
+values(10002, 'BMW X5', 3, 'Used car with 3 previous owners');
 
 insert into item(id, name, cgroup,description)
-values(10003, 'in28minutes', 2, 'Learn Microservices');
+values(10003, 'City apartment', 2, 'Apartment with 140 sqm');
+
+insert into item(id, name, cgroup,description)
+values(10004, 'Blue Jeans', 1, 'Levis boot cut size 33x32');
